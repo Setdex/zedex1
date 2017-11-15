ced03bb149dedba25f29e04e1deaa2318423442e
SetDex <lageveenhendrik@gmail.com>
2017-11-15 16:07:29 +0100

    FINALLY A DAMN GOOD NAVBAR
    Thats lit dudes

--- Javascript/JSNavbar.js
+++ Javascript/JSNavbar.js
@@ -1,8 +1,7 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
+$(window).scroll(function() {
+    if($(this).scrollTop()>5) {
+        $( ".navbar-me" ).addClass("fixed-me");
     } else {
-        x.className = "topnav";
+        $( ".navbar-me" ).removeClass("fixed-me");
     }
-}
+});
